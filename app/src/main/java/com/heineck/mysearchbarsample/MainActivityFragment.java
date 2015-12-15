987cf773ca9716ddbf0f56977e4d286792b01b24
vheineck <vinicius.heineck@acotel.com>
2015-12-15 20:05:42 -0200

    Initial commit

---
+++ app/src/main/java/com/heineck/mysearchbarsample/MainActivityFragment.java
@@ -0,0 +1,30 @@
+package com.heineck.mysearchbarsample;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private static final String TAG  = "MainActivityFragment";
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
